e892093307b9d8b2c629b1defdf7eb7e9553fb43
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-09-18 19:38:45 +0200

    props 1

--- pokemon/src/components/PokemonCard.jsx
+++ pokemon/src/components/PokemonCard.jsx
@@ -1,18 +1,7 @@
-  const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  let pokemon = pokemonList[0];
-  return <figure>
-
+function PokemonCard({pokemon}) {
+  return (
+  
+  <figure>
     {pokemon.imgSrc ? (
       <img src={pokemon.imgSrc} alt={pokemon.name}/>
     ) : (
@@ -20,7 +9,7 @@ function PokemonCard() {
     )}
     <figcaption>{pokemon.name}</figcaption>
   </figure>
-  ;
+  );
 }
 
 
