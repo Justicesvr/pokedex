42708f6d328f197a12efe82f8fabdc9365c33d16
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-09-20 15:59:59 +0200

    background color

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,49 +1,50 @@
 import PokemonCard from "./components/PokemonCard"
 import NavBar from "./components/NavBar"
-import { useEffect } from "react";
-
-
+// import { useEffect } from "react";
 import { useState } from "react";
 import './App.css'
 
  const pokemonList = [
 {
-    name: "bulbasaur",
+    name: "Bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    backgroundColor: '#90ee90',
   },
   {
-    name: "charmander",
+    name: "Charmander",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      backgroundColor: '#f69169',
   },
   {
-    name: "squirtle",
+    name: "Squirtle",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    backgroundColor: '#69e8f6',
+
   },
   {
-    name: "pikachu",
+    name: "Pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    backgroundColor: '#ffff99',
   },
   {
-    name: "mew",
+    name: "Mew",
+    backgroundColor: '#9999ff',
   },
 ];
-
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  useEffect(
-    () => {
-      alert("Hello pokemon trainer !!");
-    }, [])
+  // useEffect(
+  //   () => {
+  //     alert("Hello pokemon trainer !!");
+  //   }, [])
     
-    ;
-
-
+  //   ;
   // const handleNextClick = () => {
   //   setPokemonIndex(pokemonIndex + 1);
   // }
